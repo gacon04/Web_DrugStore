21647ad7cd2be2df4f516c43cd146e749b79cc41
ngocminh04 <crushzone610@gmail.com>
2024-11-23 08:24:29 +0700

    Update

--- Web_DrugStore/Controllers/HomeController.cs
+++ Web_DrugStore/Controllers/HomeController.cs
@@ -5,15 +5,28 @@ using System.Web;
 using System.Web.Mvc;
 using PagedList;
 using PagedList.Mvc;
+using Web_DrugStore.Models;
 namespace Web_DrugStore.Controllers
 {
     public class HomeController : Controller
     {
+        DS_DBContext db = new DS_DBContext();
         public ActionResult Index()
         {
             return View();
         }
-        
-      
+        public ActionResult NewestProduct()
+        {
+            List<SanPham> sanphams = db.SanPhams.Where(prod => prod.HoatDong == true).OrderByDescending(prod => prod.NgayTao).Take(6).ToList();
+            return PartialView(sanphams);
+        }
+        public ActionResult BestSellingProduct()
+        {
+            List<SanPham> sanphams = db.SanPhams.Where(prod => prod.HoatDong == true).OrderByDescending(prod => prod.LuotMua).Take(6).ToList();
+            return PartialView(sanphams);
+        }
+
+         
+
     }
 }
