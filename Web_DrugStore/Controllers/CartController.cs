9483fd8636323edcdd1fb5ab2e5a1fdaac0e620b
ngocminh04 <crushzone610@gmail.com>
2024-12-02 20:50:38 +0700

    UpdateSomethingCrazyButNotWorkWhenCheckout

--- Web_DrugStore/Controllers/CartController.cs
+++ Web_DrugStore/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -122,10 +123,71 @@ namespace Web_DrugStore.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Checkout(DonHang dh)
+        public ActionResult Checkout(CheckOutItemVM item)
         {
-            return View();
+            var code = new { Success = false, Code = -1 };
+            if (ModelState.IsValid)
+            {
+                ShoppingCart cart = (ShoppingCart)Session["Cart"];
+                if (cart != null)
+                {
+                    DonHang dh = new DonHang();
+                    dh.TenKhachHang = item.TenKhachHang;
+                    dh.TenDuong = item.TenDuong;
+                    dh.SoNha = item.SoNha;
+                    dh.TenHuyen = item.TenHuyen;
+                    dh.DiaChiChoLam = item.DiaChiChoLam;
+                    dh.TenXa = item.TenXa;
+                    dh.TenTinh = item.TenTinh;
+                    dh.GhiChu = item.GhiChu;
+                    dh.SoDienThoai = item.SoDienThoai;
+                    dh.Email = item.Email;
+                    string pttt = item.PhuongThucThanhToan;
+                    if (pttt.Equals("VNPAY"))
+                    {
+                        dh.CachThanhToan = HinhThucThanhToan.VNPAY;
+                    }    
+                    else if (pttt.Equals("MOMO"))
+                    {
+                        dh.CachThanhToan = HinhThucThanhToan.MOMO;
+                    }
+                    else if (pttt.Equals("COD"))
+                    {
+                        dh.CachThanhToan = HinhThucThanhToan.COD;
+                    }
+                    cart.Items.ForEach(x => dh.ChiTietDonHangs.Add(new ChiTietDonHang
+                    {
+                        SanPhamId = x.SanPhamId,
+                        SoLuong = x.SoLuong,
+                        DonGia = (double)x.sanpham.DonGia,
+                    }));
+                    double tongtienhang = (double)cart.Items.Sum(x => x.TongTien);
+                    dh.TongTienHang = tongtienhang;
+                    dh.PhiVanChuyen = 20000;
+                    dh.NgayDat = DateTime.Now;
+                    dh.UserAspId = User.Identity.GetUserId();
+                    if (tongtienhang>=300000)
+                    {
+                        dh.PhiVanChuyen = 0;
+                    }
+                    dh.VAT = tongtienhang * 0.1;
+                    dh.TongHoaDon = dh.TongTienHang + dh.PhiVanChuyen + dh.VAT;
+                    dh.TrangThai = TrangThaiDonHang.ChoXacNhan;
+                    db.DonHangs.Add(dh);
+                    db.SaveChanges();
+                    code = new { Success = true, Code = 1 };
+                }
+            }
+                  
+            return Json(code);
         }
+
+        public ActionResult Partial_CheckOut() // xử lý thông tin về nhập thông tin đặt hàng
+        {
+            return PartialView();
+        }
+
+        // hiển thị ra list sản phẩm đang trong giỏ hàng lên phần thanh toán
         public ActionResult Partial_Item_Checkout()
         {
             ShoppingCart cart = (ShoppingCart)Session["Cart"];
---
+++ Web_DrugStore/ViewModel/CheckOutItemVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_DrugStore.ViewModel
+{
+    public class CheckOutItemVM
+    {
+        [Required(ErrorMessage = "Vui lòng nhập họ và tên.")]
+        [StringLength(255)]
+        public string TenKhachHang { get; set; } // Tên khách hàng
+
+        [StringLength(255)]
+        public string Email { get; set; } // Email khách hàng
+
+        [Required]
+        [StringLength(15)]
+        public string SoDienThoai { get; set; } // Số điện thoại khách hàng
+
+        [StringLength(255)]
+        public string DiaChiChoLam { get; set; }
+
+        public string SoNha { get; set; } = "";
+
+        public string TenDuong { get; set; } = "";
+
+        public string TenXa { get; set; }
+        public string GhiChu { get; set; } = "";
+        public string TenHuyen { get; set; }
+        public string TenTinh { get; set; }
+
+        public string PhuongThucThanhToan { get; set; }
+    }
+}
